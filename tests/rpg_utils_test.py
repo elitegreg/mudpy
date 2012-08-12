7da7bea493af21a6fdd506734a7115381ca21a52
Greg Mitchell <greg.mitchell@emailman.cc>
2012-08-11 21:08:33 -0400

    removed old unit tests

--- tests/rpg_utils_test.py
+++ tests/rpg_utils_test.py
@@ -16,42 +16,42 @@ class Rpg_utils_TestCase(unittest.TestCase):
     rolls = list()
     for i in range(0, 20):
       rolls.append(rpg_utils.roll_dice(1))
-    self.assertEquals(min(*rolls), 1)
-    self.assertEquals(max(*rolls), 6)
+    self.assertEqual(min(*rolls), 1)
+    self.assertEqual(max(*rolls), 6)
 
     rolls = list()
     for i in range(0, 50):
       rolls.append(rpg_utils.roll_dice(1, 20))
-    self.assertEquals(min(*rolls), 1)
-    self.assertEquals(max(*rolls), 20)
+    self.assertEqual(min(*rolls), 1)
+    self.assertEqual(max(*rolls), 20)
 
     rolls = list()
     for i in range(0, 90):
       rolls.append(rpg_utils.roll_dice(2))
-    self.assertEquals(min(*rolls), 2)
-    self.assertEquals(max(*rolls), 12)
+    self.assertEqual(min(*rolls), 2)
+    self.assertEqual(max(*rolls), 12)
 
     rolls = list()
     for i in range(0, 1700):
       rolls.append(rpg_utils.roll_dice(4, keep=3))
-    self.assertEquals(min(*rolls), 3)
-    self.assertEquals(max(*rolls), 18)
+    self.assertEqual(min(*rolls), 3)
+    self.assertEqual(max(*rolls), 18)
 
   def test_dice_string_parsing(self):
     '''Tests dice string parsing'''
     (dice, constant) = rpg_utils.parse_dice_string('2D+2')
-    self.assertEquals(constant, 2)
-    self.assertEquals(dice, {6:2})
+    self.assertEqual(constant, 2)
+    self.assertEqual(dice, {6:2})
 
     (dice, constant) = \
       rpg_utils.parse_dice_string('1d20+3d4+2d4+1d20+1')
-    self.assertEquals(constant, 1)
-    self.assertEquals(dice, {20: 2, 4: 5})
+    self.assertEqual(constant, 1)
+    self.assertEqual(dice, {20: 2, 4: 5})
 
     (dice, constant) = \
       rpg_utils.parse_dice_string('15')
-    self.assertEquals(constant, 15)
-    self.assertEquals(dice, {})
+    self.assertEqual(constant, 15)
+    self.assertEqual(dice, {})
 
     self.assertRaises(RuntimeError, rpg_utils.parse_dice_string,
         '+1d2+1')
@@ -68,21 +68,21 @@ class Rpg_utils_TestCase(unittest.TestCase):
     rolls = list()
     for i in range(0, 40):
       rolls.append(s.roll())
-    self.assertEquals(min(*rolls), 4)
-    self.assertEquals(max(*rolls), 12)
+    self.assertEqual(min(*rolls), 4)
+    self.assertEqual(max(*rolls), 12)
 
     rolls = list()
     for i in range(0, 40):
       rolls.append(s.roll(modifier=-10, minimum=0))
-    self.assertEquals(min(*rolls), 0)
-    self.assertEquals(max(*rolls), 2)
+    self.assertEqual(min(*rolls), 0)
+    self.assertEqual(max(*rolls), 2)
 
     s = rpg_utils.Stat.from_dice_string('2d+1')
     rolls = list()
     for i in range(0, 50):
       rolls.append(s.roll())
-    self.assertEquals(min(*rolls), 3)
-    self.assertEquals(max(*rolls), 13)
+    self.assertEqual(min(*rolls), 3)
+    self.assertEqual(max(*rolls), 13)
 
 
 if __name__ == '__main__':
